607f503566fe47a064786d660333b543c7adafbf
najimhaq <mdnajimulhaque@gmail.com>
2026-05-01 23:35:33 +0900

    config file edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  reactCompiler: true,
-  swcMinify: true,
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
